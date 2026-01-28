e7e4cc2e15bc758000d7a5e41e37feb34a7107ab
Giovanni Brandolise <giovanni.brandolise@mps.com.br>
2026-01-28 15:53:34 -0300

    Create a Cache Service and Account Entity for the dataBase

---
+++ KrtBank.Domain/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿using KrtBank.Domain.Common;
+using KrtBank.Domain.Enums;
+
+namespace KrtBank.Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string AccountHolderName { get; private set; }
+        public string Cpf { get; private set; }
+        public AccountStatus AccountStatus { get; private set; }
+
+        // Construtor vazio para o Entity Framework
+        protected Account() { }
+
+        public Account(string accountHolderName, string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(accountHolderName)) throw new ArgumentException("Nome do titular é obrigatório.");
+            if (string.IsNullOrWhiteSpace(cpf)) throw new ArgumentException("CPF é obrigatório.");
+
+            AccountHolderName = accountHolderName;
+            Cpf = cpf;
+            AccountStatus = AccountStatus.Active;
+        }
+
+        public void UpdateStatus(AccountStatus newStatus)
+        {
+            AccountStatus = newStatus;
+        }
+    }
+}
---
+++ KrtBank.Infrastructure/Mappings/AccountConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using KrtBank.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KrtBank.Infrastructure.Mappings
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("Accounts");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.AccountHolderName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(a => a.Cpf)
+                .IsRequired()
+                .HasMaxLength(11);
+
+            builder.Property(a => a.AccountStatus)
+                 .HasConversion<int>()
+                 .IsRequired();
+
+            // Índice para performance e redução de custo de IOPS na nuvem
+            builder.HasIndex(a => a.Cpf).IsUnique();
+        }
+    }
+}
