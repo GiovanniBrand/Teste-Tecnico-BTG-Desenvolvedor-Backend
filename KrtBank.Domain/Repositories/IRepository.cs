5de4233d311553839445bb4942804248615d6e2b
Giovanni Brandolise <92755235+GiovanniBrand@users.noreply.github.com>
2026-01-28 14:00:41 -0300

    Add generic repositories and user entity (#2)

---
+++ KrtBank.Domain/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace KrtBank.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellation);
+        Task<T?> GetFirstAsync(Expression<Func<T, bool>> filter, CancellationToken cancellation);
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>>? filter, CancellationToken cancellation);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> filter, CancellationToken cancellation);
+        Task<int> CountAsync(Expression<Func<T, bool>> filter, CancellationToken cancellation);
+
+        Task AddAsync(T item, CancellationToken cancellation);
+        void Update(T item);
+        void Delete(T item);
+        Task<int> SaveChangesAsync(CancellationToken cancellation);
+    }
+}
